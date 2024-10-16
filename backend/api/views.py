3e729a4e1ec7dd5a889cba45009d880f93452ece
vegitobluefan <daniararinov995@gmail.com>
2024-10-16 16:25:43 +0300

    поменял юрлы и добавил вьюсет

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import Ingredient, Recipe, Tag, User
 from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 
@@ -11,7 +12,7 @@ from .paginators import CustomHomePagination
 from .permissions import IsAuthenticatedAndAdminOrAuthorOrReadOnly
 from .serializers import (IngredientSerializer, MyUserCreateSerializer,
                           RecipeSerializer, TagSerializer,
-                          UserAccessTokenSerializer)
+                          UserAccessTokenSerializer, UserSerializer)
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -37,6 +38,30 @@ class RecipeViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = RecipeSerializer
     permission_classes = (IsAuthenticatedAndAdminOrAuthorOrReadOnly,)
     pagination_class = CustomHomePagination
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели User."""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = permissions.IsAuthenticated
+    filter_backends = DjangoFilterBackend
+    lookup_field = 'username'
+    search_fields = ['username', ]
+    http_method_names = ('get', 'post', 'head', 'patch', 'delete',)
+
+    @action(methods=['patch', 'get'], detail=False,
+            permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = UserSerializer(self.request.user)
+        else:
+            serializer = UserSerializer(
+                self.request.user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=request.user.role, partial=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])

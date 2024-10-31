a333f53c537ac3da16726981def2848266bb8e11
vegitobluefan <daniararinov995@gmail.com>
2024-10-31 18:17:07 +0300

    осталось список избранного список подписок короткие ссылки картинки

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from .paginators import CustomHomePagination
 from .permissions import IsAuthenticatedAndAdminOrAuthorOrReadOnly

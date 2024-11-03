3e92f6dc0fd4f77768479b506e634362ab1556d2
vegitobluefan <daniararinov995@gmail.com>
2024-11-03 15:33:14 +0300

    на кондициях

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import transaction
-from djoser.serializers import UserCreateSerializer
 from recipes.models import (FavoriteRecipe, Ingredient, IngredientRecipe,
                             MyUser, Recipe, ShoppingCart, SubscriptionUser,
                             Tag, models)
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import (FavoriteRecipe, Ingredient, MyUser, Recipe, ShoppingCart,
                      SubscriptionUser, Tag)
@@ -7,10 +8,18 @@ admin.site.register(MyUser)
 admin.site.register(SubscriptionUser)
 admin.site.register(FavoriteRecipe)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
 admin.site.register(ShoppingCart)
 admin.site.register(Tag)
 
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'recipe_favorite',)
+
+    @admin.display(
+            description=format_html('<strong>Рецепт в избранных</strong>'))
+    def recipe_favorite(self, Recipe):
+        return Recipe.favorite_recipe.count()
 # Данные от админки:
 # Логин: daniyar
 # Пароль: arinov1701
